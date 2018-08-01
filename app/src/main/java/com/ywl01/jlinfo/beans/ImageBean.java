a9eb6ded06c433f0b2cc623e1bfeb72aae133823
ywl01 <ywl01@163.com>
2018-08-01 20:01:02 +0800

    一些类的整合

---
+++ app/src/main/java/com/ywl01/jlinfo/beans/ImageBean.java
@@ -0,0 +1,11 @@
+package com.ywl01.jlinfo.beans;
+
+public class ImageBean {
+    public int id;
+    public int hostID;
+    public String imageUrl;
+    public String thumbUrl;
+    public int imageType;
+    public int insertUser;
+    public String insertTime;
+}

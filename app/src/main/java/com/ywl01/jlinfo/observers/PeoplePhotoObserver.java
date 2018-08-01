3a7851e6f59c3791f736fb6b52af8cb3feaa6735
ywl01 <ywl01@163.com>
2018-08-01 20:18:03 +0800

    一些类的整合

---
+++ app/src/main/java/com/ywl01/jlinfo/observers/PeoplePhotoObserver.java
@@ -0,0 +1,40 @@
+package com.ywl01.jlinfo.observers;
+
+import com.ywl01.jlinfo.beans.ImageBean;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ywl01 on 2017/1/24.
+ */
+
+public class PeoplePhotoObserver extends BaseObserver {
+    @Override
+    protected List<ImageBean> convert(String data) {
+        List<ImageBean> images = new ArrayList<>();
+        try {
+            JSONArray jsonArray = new JSONArray(data);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                ImageBean imageBean = new ImageBean();
+                if (jsonObject != null) {
+                    imageBean.id = jsonObject.optInt("id");
+                    imageBean.hostID = jsonObject.optInt("peopleID");
+                    imageBean.imageUrl = jsonObject.optString("photoUrl");
+                    imageBean.thumbUrl = jsonObject.optString("thumbUrl");
+                    imageBean.insertTime = jsonObject.optString("insertTime");
+                    imageBean.insertUser = jsonObject.optInt("insertUser");
+                }
+                images.add(imageBean);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return images;
+    }
+}
